f1595556961a6b9fe1329b5814aed5abec7bd782
nageom <nageom1123@naver.com>
2021-07-11 23:06:49 +0900

    commit

--- src/greedy/LifeBoat.java
+++ src/greedy/LifeBoat.java
@@ -5,27 +5,20 @@ public class LifeBoat {
 	 public static int solution(int[] people, int limit) {
 	        int answer = 0;
 	        
-	        Arrays.sort(people);
-	        //int answer = 0;
-	        int tmp = people[0];//50
-	       // boolean flag = false;
-	        int remind = limit - people[0];
-	        for (int i=0; i< people.length; i++) {
-	        	if(remind-people[i] > 0 ) {
-	        		remind -= people[i];
-	        		
-	        	}else if(remind-people[i]==0){
+	        Arrays.sort(people);//50 50 70 80
+	       
+	        int start = 0;
+	        for (int end = people.length-1 ; end>=start; end--) {
+	        	if(people[start]+ people[end] > limit) {
 	        		answer++;
-	        		remind = limit;
-	        		
-	        	}else {
+	        	}
+	        	else {
+	        		start++;
 	        		answer++;
-	        		remind = limit-people[i];
 	        	}
 	        	
 	        }
-	        
-	        
+	       
 	        
 	        return answer;
 	    }
@@ -33,9 +26,12 @@ public class LifeBoat {
 	
 	public static void main(String[] args) {
 		int[] people = {70, 80, 50};
+		int[] people2 = {70, 50, 80, 50};
+		int[] people3 = {40,40,80};
+
 		int limit = 100;
 	
-		System.out.println(solution(people, limit));
+		System.out.println(solution(people3, limit));
 		
 	}
 	
